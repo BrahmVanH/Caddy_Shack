a23f34312c29f768ff6a6350942d5874b22ba30a
Brahm V <brahmvanh@gmail.com>
2023-05-04 20:55:32 -0400

    Added allUsers query. Successfully queried all users in apollo sandbox

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,21 +3,21 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
 	type User {
 		_id: ID!
-		first_name: String!
-		last_name: String!
+		firstName: String!
+		lastName: String!
 		username: String!
 		age: Int!
 		gender: String!
 		genderInterest: String!
 		bio: String!
 		saidYesTo: [User]
-    messages: [Message]
+		messages: [Message]
 	}
 
 	input UserInput {
 		_id: ID!
-		first_name: String!
-		last_name: String!
+		firstName: String!
+		lastName: String!
 		username: String!
 		age: Int!
 		gender: String!
@@ -25,16 +25,24 @@ const typeDefs = gql`
 		bio: String!
 	}
 
-  type Message {
-    _id: ID!
-    messageSender: String!
-    messageRecipient: String!
-    messageBody: String!
-    createdAt: String!
+	type Message {
+		_id: ID!
+		messageSender: String!
+		messageRecipient: String!
+		messageBody: String!
+		createdAt: String!
+	}
 
-  }
+	type Query {
+		# user(username: String!): User
+		allUsers: [User]
+	}
 
-
+	# type Mutation {
+	# 	createUser()
+	# }
 `;
 
 // To-do: Add queries, add mutation
+
+module.exports = typeDefs;
--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -6,10 +6,8 @@ const userSeeds = require('./userSeeds.json');
 db.once('open', async () => {
 	try {
 		await User.deleteMany({});
-		// await Book.deleteMany({});
 
 		await User.create(userSeeds);
-		// await Book.create(bookSeeds);
 	} catch (err) {
 		console.error(err);
 		process.exit(1);
