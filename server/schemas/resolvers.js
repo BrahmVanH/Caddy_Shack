a6e4902a23bd127fe818df4447febae5f9acd5db
Brahm V <brahmvanh@gmail.com>
2023-05-07 13:22:13 -0400

    Created mutation type definitions

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,31 +2,31 @@ const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 
 const resolvers = {
-  Query: {
-    allUsers: async () => {
-      const allUsers = await User.find({});
-      console.log(allUsers);
-      if (!allUsers) {
-        throw new Error('Something went wrong');
-      }
+	Query: {
+		allUsers: async () => {
+			const allUsers = await User.find({});
+			console.log(allUsers);
+			if (!allUsers) {
+				throw new Error('Something went wrong');
+			}
 
-      return allUsers;
-    },
-    allMen: async () => {
-      const users = await User.find({ gender: 'male' });
-      if (!users) {
-        throw new Error('Something went wrong');
-      }
-      return users;
-    },
-    allWomen: async () => {
-      const users = await User.find({ gender: 'female' });
-      if (!users) {
-        throw new Error('Something went wrong');
-      }
-      return users;
-    }
-  }
-}
+			return allUsers;
+		},
+		allMen: async () => {
+			const users = await User.find({ gender: 'male' });
+			if (!users) {
+				throw new Error('Something went wrong');
+			}
+			return users;
+		},
+		allWomen: async () => {
+			const users = await User.find({ gender: 'female' });
+			if (!users) {
+				throw new Error('Something went wrong');
+			}
+			return users;
+		},
+	},
+};
 
 module.exports = resolvers;
