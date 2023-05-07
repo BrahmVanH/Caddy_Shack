8945025f21f7cda24f083434b2af761db543109d
Thomas Newnum <thomas.newnum@gmail.com>
2023-05-06 20:10:13 -0400

    now they actually work

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/img/logo.png";
 import "./assets/css/signup.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Signup() {
   return (
@@ -37,7 +37,7 @@ function Signup() {
                       <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
                     </svg>
                   </div>
-                  <Link to="/Profile">
+
                   <form className="text-center" method="post">
                     <div className="mb-3">
                       <input
@@ -60,12 +60,11 @@ function Signup() {
                         className="btn btn-primary d-block w-100"
                         type="submit"
                       >
-                        Login
+                        <Link className="myBtn" to="/Profile">Login</Link>
                       </button>
                     </div>
                     <p className="text-muted">Forgot your password?</p>
                   </form>
-                  </Link>
                 </div>
               </div>
             </div>
@@ -94,34 +93,34 @@ function Signup() {
                       <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
                     </svg>
                   </div>
-                  <Link to="/register">
-                    <form className="text-center" method="post">
-                      <div className="mb-3">
-                        <input
-                          className="form-control"
-                          type="email"
-                          name="email"
-                          placeholder="Email"
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <input
-                          className="form-control"
-                          type="password"
-                          name="password"
-                          placeholder="Password"
-                        />
-                      </div>
-                      <div className="mb-3">
-                        <button
-                          className="btn btn-primary d-block w-100"
-                          type="submit"
-                        >
+                  <form className="text-center" method="post">
+                    <div className="mb-3">
+                      <input
+                        className="form-control"
+                        type="email"
+                        name="email"
+                        placeholder="Email"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <input
+                        className="form-control"
+                        type="password"
+                        name="password"
+                        placeholder="Password"
+                      />
+                    </div>
+                    <div className="mb-3">
+                      <button
+                        className="btn btn-primary d-block w-100"
+                        type="submit"
+                      >
+                        <Link className="myBtn" to="/register">
                           Signup
-                        </button>
-                      </div>
-                    </form>
-                  </Link>
+                        </Link>
+                      </button>
+                    </div>
+                  </form>
                 </div>
               </div>
             </div>
