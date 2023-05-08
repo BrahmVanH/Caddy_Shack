baf15b2e30ab8a93febf2dd22c68873249720e37
Thomas Newnum <thomas.newnum@gmail.com>
2023-05-08 16:09:56 -0400

    input area looking good

--- client/src/Pages/Message.js
+++ client/src/Pages/Message.js
@@ -1,4 +1,6 @@
 import logo from "./assets/img/logo.png"
+import "./assets/css/message.css"
+
 function Message() {
     return (
       <div>
@@ -9,6 +11,31 @@ function Message() {
           </a>
         </div>
       </nav>
+      <div class="chatbox">
+        <div class="chats">
+        
+
+        </div>
+    </div>
+    <div className="container">
+      <div className="input-group mb-3 textArea">
+        <input
+          type="text"
+          className="form-control text-input"
+          placeholder="Your Message Here"
+          aria-label="Recipient's username"
+          aria-describedby="button-addon2"
+        />
+        <button
+          className="btn btn-secondary btn-outline-secondary myBtn"
+          type="button"
+          id="button-addon2"
+        >
+          Send
+        </button>
+      </div>
+    </div>
+
 
 
 
