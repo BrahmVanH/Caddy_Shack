324991dae6288e3e19140ed78fcc6eb2d2cf7267
Thomas Newnum <thomas.newnum@gmail.com>
2023-05-08 13:10:21 -0400

    startting message center

--- client/src/Pages/Message.js
+++ client/src/Pages/Message.js
@@ -1,7 +1,26 @@
 function Message() {
     return (
       <div>
-     Message Page
+     <nav className="lostNavbar navbar-expand-md">
+        <div className="container">
+          <a className="navbar-brand d-flex align-items-center" href="/">
+            <img id="logo" src={logo} width="75px" alt="logo" />
+          </a>
+        </div>
+      </nav>
+
+
+
+
+      <footer id="lostFooter" className="text-center">
+        <p className="slogan">
+          Find Love that is on Par ⛳ Even if your golf game isn't!
+        </p>
+      </footer>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        crossOrigin="anonymous"
+      ></script>
       </div>
     );
   }
