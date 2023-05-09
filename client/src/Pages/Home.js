e2c1f8dad96d4b4d0161cd5eeb18b0df3bfcdaca
Thomas Newnum <thomas.newnum@gmail.com>
2023-05-09 11:28:24 -0400

    minor tweaks to style, and correction of naming - starting to work on matching page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -2,7 +2,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/home.css";
 
 function Home() {
-  return;
+  return (
+    <>
+    <section className="homeContainer">
+
+     <p className="home">
+      Caddy Shack is a dating app specifically designed for golf enthusiasts
+      looking for love. Whether you are a seasoned pro or a beginner, Caddy
+      Shack is the perfect place to find a partner who shares your passion for
+      golf.
+    </p> 
+    </section>
+  </>
+
+  );
 }
 
 export default Home;
