073ed1a42a5b4b3731ea7143af5a94c62cc15e80
Thomas Newnum <thomas.newnum@gmail.com>
2023-05-09 13:21:35 -0400

    matching broken out to its own component

--- client/src/Pages/Matching.jsx
+++ client/src/Pages/Matching.jsx
@@ -1,25 +1,10 @@
-import gopher from "./assets/img/gopher.png";
-import "./assets/css/matching.css";
+import PotentialMatch from "./components/Matching/PotentialMatch.jsx";
 
 function Matching() {
   return (
-    <div>
-      <div className="myCard">
-        <div className="card" style={{ width: "18rem" }}>
-          <img src={gopher} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">User Name</h5>
-            <p className="card-text">User's Bio</p>
-            <a href="/" className="btn btn-success myBtn">
-              YES
-            </a>
-            <a href="/" className="btn btn-danger myBtn">
-              No
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+   <PotentialMatch />
+    </>
   );
 }
 
