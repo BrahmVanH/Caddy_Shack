be126a6be63b2e758c062f7a95aeedc341a84912
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-05-10 06:11:40 -0400

    change lincontainter to link

--- client/src/Pages/components/Matching/PotentialMatch.jsx
+++ client/src/Pages/components/Matching/PotentialMatch.jsx
@@ -1,29 +1,30 @@
-// import gopher from "../../assets/img/gopher.png";
-// import "../Matching/potential.css";
-// import { LinkContainer } from "react-router-bootstrap";
+import gopher from "../../assets/img/gopher.png";
+import "../Matching/potential.css";
+import { Link } from "react-router-dom";
 
-// function PotentialMatch() {
-//   return (
-//     <div>
-//       <div className="myCard">
-//         <div className="card" style={{ width: "18rem" }}>
-//           <img src={gopher} className="card-img-top" alt="..." />
-//           <div className="card-body">
-//             <h5 className="card-title">User Name</h5>
-//             <p className="card-text">User's Bio</p>
-//             <LinkContainer to="/">
-//               <a href="/" className="btn btn-success myBtn">
-//                 YES
-//               </a>
-//             </LinkContainer>
-//             <a href="/" className="btn btn-danger myBtn">
-//               No
-//             </a>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 
-// export default PotentialMatch;
+function PotentialMatch() {
+  return (
+    <div>
+      <div className="myCard">
+        <div className="card" style={{ width: "18rem" }}>
+          <img src={gopher} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">User Name</h5>
+            <p className="card-text">User's Bio</p>
+            <Link to="/">
+              <a href="/" className="btn btn-success myBtn">
+                YES
+              </a>
+            </Link>
+            <a href="/" className="btn btn-danger myBtn">
+              No
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default PotentialMatch;
