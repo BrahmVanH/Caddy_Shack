3ed139d9e1109c0532ab254cec02ca3448cbfa86
Theophilus <93491747+nwherman1724@users.noreply.github.com>
2023-05-14 21:52:28 -0400

    matching test

--- client/src/components/Matching/PotentialMatch.jsx
+++ client/src/components/Matching/PotentialMatch.jsx
@@ -1,8 +1,10 @@
-// import gopher from "../../assets/img/gopher.png";
-// import "../Matching/potential.css";
-// import { Link } from "react-router-dom";
-// // import Auth from "../../../utils/auth"
-// import { gql, useQuery } from '@apollo/client';
+import gopher from "../../assets/img/gopher.png";
+import "../Matching/potential.css";
+import { Link } from "react-router-dom";
+// import Auth from "../../utils/auth";
+import { gql, useQuery } from '@apollo/client';
+import TinderCard from 'react-tinder-card';
+import { useState } from 'react';
 
 
 
@@ -14,11 +16,58 @@
 //     }
 // `;
 
-// function PotentialMatch() {
-//   console.log(Auth.getProfile().data._id)
-//   const { loading, error, data } = useQuery(GET_INTEREST, {
-//     variables: { userId: Auth.getProfile().data._id}
-//   });
+
+
+function PotentialMatch() {
+
+    const characters = [
+        {
+          name: 'Richard Hendricks',
+          url: 'https://i.imgur.com/QsjSMCZ.png',
+          gender: "male",
+          genderInterest: 'female'
+        },
+        {
+          name: 'Erlich Bachman',
+          url: 'https://i.imgur.com/QsjSMCZ.png',
+          gender: "male",
+          genderInterest: 'female'
+        },
+        {
+          name: 'Monica Hall',
+          url: 'https://i.imgur.com/QsjSMCZ.png',
+          gender: "female",
+          genderInterest: 'male'
+        },
+        {
+          name: 'Jared Dunn',
+          url: 'https://i.imgur.com/QsjSMCZ.png',
+          gender: "male",
+          genderInterest: 'female'
+        },
+        {
+          name: 'Sarah Chugtai',
+          url: 'https://i.imgur.com/QsjSMCZ.png',
+          gender: "female",
+          genderInterest: 'male'
+        }
+      ]
+
+    const [lastDirection, setLastDirection] = useState()
+  
+    const swiped = (direction, nameToDelete) => {
+      console.log('removing: ' + nameToDelete)
+      setLastDirection(direction)
+    }
+  
+    const outOfFrame = (name) => {
+      console.log(name + ' left the screen!')
+    }
+
+//   const { loading, error, data } = useQuery(GET_INTEREST
+//     , {
+//     variables: { userId: Auth.getProfile().data._id}}
+//     );
 
 //   if (loading) {
 //     return <div>Loading Your Matches!</div>;
@@ -27,32 +76,39 @@
 //   if (error) {
 //     return <div>There was an error loading your matches: {error.message}</div>;
 //   }
-// // Need to add buttons to like or swipe user
+// Need to add buttons to like or swipe user
 
-// return (
-//     <div>
-//       <div className="myCard">
-//         <div className="card" style={{ width: "18rem" }}>
-//           <img src={gopher} className="card-img-top" alt="..." />
-//           <div className="card-body">
-//             <h5 className="card-title">User Name</h5>
-//             <p className="card-text">User's Bio</p>
-           
-//               <span className="btn btn-success myBtn">
-//                 YES
-//               </span>
-            
+return (
+        <div className="swiper-container">
+          <div className="tindercard-container">
+            {characters.map((character) => {
+              if (character.gender === "female") {
+                const femaleChar = [];
+                femaleChar.push(
+                  <TinderCard
+                    className="swipe"
+                    key={character.name}
+                    onSwipe={(dir) => swiped(dir, character.name)}
+                    onCardLeftScreen={() => outOfFrame(character.name)}
+                  >
+                    <div
+                      style={{ backgroundImage: "url(" + character.url + ")" }}
+                      className="tindercard"
+                    >
+                      <h3>{character.name}</h3>
+                    </div>
+                  </TinderCard>
+                );
+                return femaleChar;
+              }
+              return null; // Or any alternative fallback value if no conditions are met
+            })}
+          </div>
+          <div className="swipe-info">
+            {lastDirection && <p>You swiped {lastDirection}.</p>}
+          </div>
+        </div>
+  );
+};
 
-//             <span className="btn btn-danger myBtn">
-//               No
-//             </span>
-            
-
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default PotentialMatch;
+export default PotentialMatch;
